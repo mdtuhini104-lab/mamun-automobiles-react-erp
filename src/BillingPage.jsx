9d6c48af7d2117d58c9e02ff737968d2c9172596
mdtuhini104-lab <mdtuhini104@gmail.com>
2026-05-16 13:43:14 +0600

    Update BillingPage.jsx

--- src/BillingPage.jsx
+++ src/BillingPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { bufferedPrint } from './utils/printAssistant';
 import { createPortal } from 'react-dom';
-import { Card, Table, Button, Input, Select, DatePicker, Row, Col, Typography, InputNumber, Modal, Form, Space, message, Radio, Divider, Tabs, AutoComplete, Tag, Checkbox, Avatar, Alert, Tooltip, List } from 'antd'; Divider } from 'antd';
+import { Card, Table, Button, Input, Select, DatePicker, Row, Col, Typography, InputNumber, Modal, Form, Space, message, Radio, Divider, Tabs, AutoComplete, Tag, Checkbox, Avatar, Alert, Tooltip, List } from 'antd'; Divider } from 'antd'; Divider } from 'antd';
 import { useGlobalState } from './contexts/GlobalStateContext';
 import { useAuth } from './contexts/AuthContext';
 import { Mic, MicOff, Sparkles } from 'lucide-react';
